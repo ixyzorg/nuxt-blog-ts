992e6953caa7c1ed1ad8cd372556aa273f06b31a
ixyz <m@ixyz.org>
2024-06-30 23:43:01 +0800

    项目基本结构

---
+++ pages/pic-bed.vue
@@ -0,0 +1,14 @@
+<template>
+ <main class="min-h-screen">
+  <AppHeader class="mb-8" title="图床" :description="description" />
+  <div class="relative" ref="imgWall" />
+ </main>
+</template>
+
+<script setup lang="ts">
+  const description = "🌁功能开发中,敬请期待...";
+  useSeoMeta({
+    title: "图床 | ixyz",
+    description,
+  });
+</script>
